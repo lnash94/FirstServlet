bdcd1d60f38d55563f31034a5c85b5ce0232d5d5
lnash94 <lnash94@users.noreply.github.com>
2017-02-26 10:45:56 +0530

    Add files via upload

--- WEB-INF/classes/LoginServlet.java
+++ WEB-INF/classes/LoginServlet.java
@@ -1,1 +1,33 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+//import java.sql.*;
+//import java.util.*;
+ 
+public class LoginServlet extends HttpServlet{
+	public String message;
+	public void init() throws ServletException{
+		message="hello world servlet";
+	}
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		
+		PrintWriter out = response.getWriter();
 
+		out.println("<html>");
+      	out.println("<head>");
+      	out.println("<title> Hello World </title>");
+      	out.println("<body>");
+     	out.println("<h3>" + message+ "</h3>");
+     	out.println("</body>");
+        out.println("</html>");
+
+
+	}
+	public void destroy()
+	{
+		
+	}
+}
